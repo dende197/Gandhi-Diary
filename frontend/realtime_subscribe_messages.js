a68934b678b2a2ccce047699734b211612aff716
andrea <andrea.altobello22@gmail.com>
2026-02-04 21:42:16 +0100

    FIX-ALL: Corrected Supabase Key syntax

--- frontend/realtime_subscribe_messages.js
+++ frontend/realtime_subscribe_messages.js
@@ -2,7 +2,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // CONFIGURA QUI: URL del progetto e ANON KEY
 const SUPABASE_URL = 'https://mlcutgkfunbpmrnbeznd.supabase.co';
-const SUPABASE_ANON_KEY = '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sY3V0Z2tmdW5icG1ybmJlem5kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxOTg2NDgsImV4cCI6MjA4NDc3NDY0OH0.eWR7PxNsJjSGAM1WoaNseVkeQDpEqaUvO8xvXoDKLQg>'; // <-- incolla la tua anon key
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sY3V0Z2tmdW5icG1ybmJlem5kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxOTg2NDgsImV4cCI6MjA4NDc3NDY0OH0.eWR7PxNsJjSGAM1WoaNseVkeQDpEqaUvO8xvXoDKLQg'; // <-- incolla la tua anon key
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
