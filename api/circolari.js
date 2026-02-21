1c5b7a93d618cce72e6c739cd7805bbf61d8e938
dende197 <andrea.altobello22@gmail.com>
2026-02-21 18:04:06 +0100

    Update circolari.js

--- api/circolari.js
+++ api/circolari.js
@@ -1,5 +1,9 @@
 module.exports = async function handler(req, res) {
-    const action = req.url.split('?')[0].includes('/sintesi') ? 'sintesi' : 'index';
-    if (action === 'sintesi') return require('../api_internal/circolari/sintesi')(req, res);
+    const url = new URL(req.url, 'http://localhost');
+    const action = url.searchParams.get('action') || 'index';
+    
+    if (action === 'sintesi') {
+        return require('../api_internal/circolari/sintesi')(req, res);
+    }
     return require('../api_internal/circolari/index')(req, res);
 };
